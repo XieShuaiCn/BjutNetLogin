5cc4a8c54eba5b2d3d36bb9a32bf8f9f848742ac
hrrcn <xs1102@sina.com>
2018-06-13 15:54:46 +0800

    C#语言开发，适用于windows .net4.0+

---
+++ LgnBjutEduCN/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace LgnBjutEduCN
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem ExitToolStripMenuItem;
+        private ToolStripMenuItem RefreshToolStripMenuItem;
+        private ToolStripMenuItem AboutToolStripMenuItem;
+        private NotifyIcon notify;
+        private WebBrowser web;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            this.notify.Visible = false;
+            base.Dispose(disposing);
+        }
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ExitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.RefreshToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.AboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.notify = new NotifyIcon();
+            this.web = new WebBrowser();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ExitToolStripMenuItem,
+            this.RefreshToolStripMenuItem,
+            this.AboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(810, 25);
+            this.menuStrip1.TabIndex = 0;
+            // 
+            // 退出ToolStripMenuItem
+            // 
+            this.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem";
+            this.ExitToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.ExitToolStripMenuItem.Text = "退出";
+            this.ExitToolStripMenuItem.Click += new EventHandler(ExitToolStripMenuItem_Click);
+            // 
+            // 刷新ToolStripMenuItem
+            // 
+            this.RefreshToolStripMenuItem.Name = "RefreshToolStripMenuItem";
+            this.RefreshToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.RefreshToolStripMenuItem.Text = "刷新";
+            this.RefreshToolStripMenuItem.Click += new EventHandler(RefreshToolStripMenuItem_Click);
+            // 
+            // 关于ToolStripMenuItem
+            // 
+            this.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem";
+            this.AboutToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.AboutToolStripMenuItem.Text = "关于";
+            this.AboutToolStripMenuItem.Click += new EventHandler(AboutToolStripMenuItem_Click);
+            //
+            this.web.Dock = DockStyle.Fill;
+            //
+            this.notify.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.notify.Text = "登陆BJUT网关";
+            this.notify.Visible = true;
+            this.notify.DoubleClick += new EventHandler(notify_DoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(850, 530);
+            this.MinimumSize = new System.Drawing.Size(850, 530);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.web);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "登陆BJUT网关";
+            this.ShowInTaskbar = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.FormClosing += new FormClosingEventHandler(Form1_FormClosing);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            web.Navigate("http://lgn.bjut.edu.cn");
+        }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = MessageBox.Show("是否退出登录账号？", "退出", MessageBoxButtons.YesNoCancel);
+            if (dr == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+            else if (dr == DialogResult.No)
+            {
+                Application.Exit();
+            }
+            else if(dr == DialogResult.Cancel)
+            {
+                return;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                e.Cancel = true;
+                this.Hide();
+            }
+        }
+
+        private void notify_DoubleClick(object sender, EventArgs e)
+        {
+            this.Show();
+            this.Activate();
+        }
+
+        private void RefreshToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.web.Refresh();
+        }
+
+        private void AboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("北工大 网关登陆器\r\n用于学校的网关登录并保持。\r\n\r\nCopyright 2017 信息学部 谢帅 版权所有");
+        }
+    }
+}
